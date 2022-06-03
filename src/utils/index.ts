e25b623d72303507f1547a61eda44c89aba13b02
Snxy <892573319@qq.com>
2022-06-04 00:39:56 +0800

    chore: any -> unknow

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value: any) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 export const cleanObject = (object: object) => {
   const result = { ...object };
 
@@ -20,7 +20,8 @@ export const useMount = (callback: () => void) => {
     callback();
   }, []);
 };
-export const useDebounce = (value: any, delay?: number) => {
+
+export const useDebounce = (value: unknown, delay?: number): any => {
   const [debounceValue, setDebounceValue] = useState(value);
 
   useEffect(() => {
