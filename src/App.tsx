c841e5bac2599b7219a8cec8cf0a0d819af09056
Snxy <892573319@qq.com>
2022-06-05 18:55:15 +0800

    feat: 添加登录

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ProjectListScreen } from 'srceens/project-list';
+// import { ProjectListScreen } from 'srceens/project-list';
+import { LoginScreen } from 'srceens/login';
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
+      <LoginScreen />
     </div>
   );
 }
