61666ee4a7f3508994f4a62d088562ed78516b37
Snxy <892573319@qq.com>
2022-06-27 14:55:27 +0800

    fix: 改变setSearchParams的实现方式,修复创建项目无法关闭

--- src/screens/project-list/util.ts
+++ src/screens/project-list/util.ts
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import { useProject } from "utils/project";
-import { useUrlQueryParam } from "utils/url";
+import { useSetUrlSearchParam, useUrlQueryParam } from "utils/url";
 
 // 项目列表搜索的参数
 export const useProjectsSearchParams = () => {
@@ -29,15 +29,16 @@ export const useProjectModal = () => {
   const [{ editingProjectId }, setEditingProjectId] = useUrlQueryParam([
     "editingProjectId",
   ]);
+  const setUrlParams = useSetUrlSearchParam();
   const { data: editingProject, isLoading } = useProject(
     Number(editingProjectId)
   );
 
   const open = () => setProjectCreate({ projectCreate: true });
-  const close = () => {
-    setProjectCreate({ projectCreate: undefined });
-    setEditingProjectId({ editingProjectId: undefined });
-  };
+  const close = () => setUrlParams({ projectCreate: "", editingProjectId: "" });
+  // setProjectCreate({ projectCreate: undefined });
+  // setEditingProjectId({ editingProjectId: undefined });
+
   const startEdit = (id: number) =>
     setEditingProjectId({ editingProjectId: id });
   return {
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -6,8 +6,9 @@ import { cleanObject } from "utils";
  */
 
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
-  const [searchParams, setSearchParam] = useSearchParams();
   // as { [key in K]: string } 取得泛型的值作为key
+  const [searchParams] = useSearchParams();
+  const setSearchParams = useSetUrlSearchParam();
   return [
     useMemo(
       () =>
@@ -18,11 +19,18 @@ export const useUrlQueryParam = <K extends string>(keys: K[]) => {
       [searchParams]
     ),
     (params: Partial<{ [key in K]: unknown }>) => {
-      const o = cleanObject({
-        ...Object.fromEntries(searchParams),
-        ...params,
-      }) as URLSearchParamsInit;
-      return setSearchParam(o);
+      return setSearchParams(params);
     },
   ] as const;
+};
+
+export const useSetUrlSearchParam = () => {
+  const [searchParams, setSearchParam] = useSearchParams();
+  return (params: { [key in string]: unknown }) => {
+    const o = cleanObject({
+      ...Object.fromEntries(searchParams),
+      ...params,
+    }) as URLSearchParamsInit;
+    return setSearchParam(o);
+  };
 };
